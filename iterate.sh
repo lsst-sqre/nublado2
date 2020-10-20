8573d630879a2fb42e06fb8fbba9c974fe16f752
Christine Banek <cbanek@gmail.com>
2020-10-20 15:00:01 -0700

    [DM-27044] Fix iterate.sh script
    Now that I've pushed the chart, I can use this and fix it so that
    it includes the lsstsqre part!

--- iterate.sh
+++ iterate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 helm delete --purge n2-dev || true
 docker build -t lsstsqre/nublado2:dev .
-helm upgrade --install n2-dev nublado2 --namespace n2-dev --values dev-values.yaml
+helm upgrade --install n2-dev lsstsqre/nublado2 --namespace n2-dev --values dev-values.yaml
