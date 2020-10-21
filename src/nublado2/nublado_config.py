2e90508e85aa7e5fb12fb798b15be6ee7dbcc794
Christine Banek <cbanek@gmail.com>
2020-10-21 11:10:43 -0700

    [DM-27245] Consistent naming of nublado_config.yaml
    Getting more consistent about using nublado_config.yaml to specify
    the nublado parts of the config, as opposed to the hub which also
    takes a config.

--- src/nublado2/nublado_config.py
+++ src/nublado2/nublado_config.py
@@ -10,7 +10,7 @@ from traitlets.config import LoggingConfigurable
 
 class NubladoConfig(LoggingConfigurable):
     def get(self) -> Dict[str, Any]:
-        with open("/etc/jupyterhub/hub_config.yaml") as f:
+        with open("/etc/jupyterhub/nublado_config.yaml") as f:
             nc = yaml.load(f.read(), yaml.FullLoader)
 
         self.log.debug(f"Loaded Nublado Config:\n{nc}")
