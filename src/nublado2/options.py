7eb2d728f7f6485067052a6ed53cf4f4dddfb8c5
Christine Banek <cbanek@gmail.com>
2020-11-03 17:23:53 -0700

    [DM-27300] Add a basic options form
    This looks mostly like the old one, except the data is static
    and pulled from the nublado config form.  This doesn't hook up
    the using it part yet, which is next.

--- src/nublado2/options.py
+++ src/nublado2/options.py
@@ -1,13 +1,59 @@
+from jinja2 import Template
 from traitlets.config import LoggingConfigurable
+
+from nublado2.nublado_config import NubladoConfig
+
+options_template = Template(
+    """
+<style>
+    td {
+        border: 1px solid black;
+        padding: 2%;
+        vertical-align: top;
+    }
+</style>
+
+<table width="100%">
+<tr>
+  <th>Image</th>
+  <th>Options</th>
+</tr>
+
+<tr>
+
+<td width="50%">
+{% for i in images %}
+    <input type="radio" name="image"
+     id="{{ i.name }}" value="{{ i.name }}"
+     {% if loop.first %} checked {% endif %}
+    >
+    {{ i.name }}<br />
+{% endfor %}
+</td>
+
+<td width="50%">
+{% for s in sizes %}
+    <input type="radio" name="size"
+     id="{{ s.name }}" value="{{ s.name }}"
+     {% if loop.first %} checked {% endif %}
+    >
+    {{ s.name }} ({{ s.cpu }} CPU, {{ s.ram }} RAM)<br>
+{% endfor %}
+
+    <br>
+    <input type="checkbox"
+     name="enable_debug" value="true">Enable debug logs<br>
+</td>
+
+</tr>
+</table>
+"""
+)
 
 
 class NubladoOptions(LoggingConfigurable):
     def show_options_form(self, spawner) -> str:
-        return """
-<div class="form-group">
-    <input type="radio" id="image-ver-1" name="image" value="image-ver-1">
-    <label for="image-ver-1">Version 1</label><br>
-    <input type="radio" id="image-ver-2" name="image" value="image-ver-2">
-    <label for="image-ver-2">Version 2</label><br>
-</div>
-        """
+        options_config = NubladoConfig().get()["options_form"]
+        images = options_config["images"]
+        sizes = options_config["sizes"]
+        return options_template.render(images=images, sizes=sizes)
