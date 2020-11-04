2c1fb6165941775a79251f91df456b52214723a1
Christine Banek <cbanek@gmail.com>
2020-11-03 19:43:18 -0700

    [DM-27300] Process and apply options from form
    Apply the image, size, and debug flag.

--- src/nublado2/hooks.py
+++ src/nublado2/hooks.py
@@ -1,6 +1,7 @@
 from jupyterhub.spawner import Spawner
 from traitlets.config import LoggingConfigurable
 
+from nublado2.nublado_config import NubladoConfig
 from nublado2.options import NubladoOptions
 from nublado2.resourcemgr import ResourceManager
 
@@ -16,6 +17,23 @@ class NubladoHooks(LoggingConfigurable):
         self.log.debug(
             f"Pre-spawn hook called for {user} with options {options}"
         )
+
+        # Look up what the user selected on the options form.
+        # Each parameter comes back as a list, even if only one is
+        # selected.
+        size_name = options["size"][0]
+        image_name = options["image"][0]
+
+        # Take size and image names, which are returned as form data,
+        # look up associated values, and configure the spawner.
+        # This will help set up the created lab pod.
+        nc = NubladoConfig()
+        (cpu, ram) = nc.lookup_size(size_name)
+        spawner.image = nc.lookup_image_url(image_name)
+        spawner.debug = options.get("debug_enabled", False)
+        spawner.mem_limit = ram
+        spawner.cpu_limit = cpu
+
         self.resourcemgr.create_user_resources(user)
 
     def post_stop(self, spawner: Spawner) -> None:
