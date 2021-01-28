4f726aebc76a69ff0579ebb460bf94e5a2acc3f3
Christine Banek <cbanek@gmail.com>
2021-01-28 14:00:29 -0700

    [DM-28482] Add namespace to iterate.sh
    Forgot the namespace part, but this kept on working due to the ||
    true.

--- iterate.sh
+++ iterate.sh
@@ -6,6 +6,6 @@ else
   CHART=cachemachine
 fi
 
-helm delete --purge n2-dev || true
+helm delete n2-dev --namespace n2-dev || true
 docker build -t lsstsqre/nublado2:dev .
 helm upgrade --install n2-dev $CHART --namespace n2-dev --values dev-values.yaml --create-namespace
